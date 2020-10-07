05f8333552cdbb41d22f6f4c7c16fc4e98ae0d8d
Mathithayani <mthayani15@gmail.com>
2020-10-07 21:22:40 +0530

    commit Visuals

---
+++ Visualization.py
@@ -0,0 +1,20 @@
+
+# ## Importing the relevant libraries
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+sns.set()
+
+
+data = pd.read_csv(R"D:\Data Reimagined\Task 1\INTERNET BANKING - Sheet1.csv")
+data.head()
+
+sns.countplot(x='Internet Banking Usage',data=data)
+
+
+sns.countplot(x='Internet Banking Usage',hue='Gender',data=data)
+
+
+sns.countplot(y="Area of living (District)", data=data)
+plt.show()
